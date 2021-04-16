9a32004e6741d146629e26e9a3d76a02b803cb45
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-04-16 17:29:33 -0300

    .gitattributes

--- color-or-image-variation-swatches-for-woocommerce.php
+++ color-or-image-variation-swatches-for-woocommerce.php
@@ -7,7 +7,7 @@ WC requires at least: 3.0.0
 WC tested up to: 5.2
 Author: Thanks to IT
 Author URI: https://github.com/thanks-to-it
-Copyright: © 2020 Thanks to IT.
+Copyright: © 2021 Thanks to IT.
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: color-or-image-variation-swatches-for-woocommerce
